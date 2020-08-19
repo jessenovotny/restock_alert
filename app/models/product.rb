a26a0ad0242544b3d87c7d63fb1f5b3e612d9e05
Jesse Novotny <jessenovotny@gmail.com>
2020-08-19 05:13:04 -0700

    fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -16,7 +16,7 @@ class Product < Airrecord::Table
 
   ALERT_DURATION = 15 # minutes
 
-  %w(url keyword keyword_count alerting alert_series_started_at).each do |attribute|
+  %w(url keyword keyword_count alerting alert_series_started_at updated_at last_alerted_at).each do |attribute|
     define_method(attribute) do
       self[attribute]
     end
