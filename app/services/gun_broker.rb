2d5154d667d906256275050dd06e76aab4c01c07
Jesse Novotny <jessenovotny@gmail.com>
2020-09-18 21:24:44 -0700

    updaing listing payload

--- app/services/gun_broker.rb
+++ app/services/gun_broker.rb
@@ -20,6 +20,31 @@ class GunBroker
     JSON.parse(res.body)['accessToken']
   end
 
+  PAYMENT_METHODS = {
+    Check: false,
+    VisaMastercard: false,
+    COD: false,
+    Escrow: false,
+    Amex: false,
+    PayPal: true,
+    Discover: false,
+    SeeItemDesc: false,
+    CertifiedCheck: false,
+    USPSMoneyOrder: false,
+    MoneyOrder: false,
+    FreedomCoin: false
+  }
+
+  SHIPPING_METHODS = {
+    Overnight: false,
+    TwoDay: false,
+    ThreeDay: false,
+    Ground: false,
+    FirstClass: false,
+    Priority: true,
+    Other: false
+  }
+
   def self.create_listing(listing)
     payload = {
       CategoryID: 3016,
@@ -30,23 +55,24 @@ class GunBroker
       InspectionPeriod: 1,
       IsFFLRequired: false,
       ListingDuration: 5,
-      PaymentMethods: { FreedomCoin: true },
+      PaymentMethods: PAYMENT_METHODS,
       PictureURLs: [ 'https://s3.amazonaws.com/backup-talend.appcohesion.io/DistributorMedia_S3/MSSBR-2NSQZKHI_LIP_MB65035.jpg' ],
       Quantity: 1,
       PostalCode: 85282,
       ShippingClassCosts: { Priority: 10.00 },
-      ShippingClassesSupported: { Priority: true },
+      ShippingClassesSupported: SHIPPING_METHODS,
       StartingBid: 0.01,
       Title: 'listing.title',
       WhoPaysForShipping: 8,
       WillShipInternational: false
     }.to_json
     headers = {
-      'X-DevKey' => GunBroker::API_KEY,
+      'X-DevKey' => API_KEY,
       'Content-Type' => 'multipart/form-data',
-      'X-AccessToken' => GunBroker.access_token
+      'X-AccessToken' => access_token
     }
-    res = RestClient.post(GunBroker::BASE_URI + '/items', payload, headers)
+    binding.pry
+    res = RestClient.post(BASE_URI + '/items', payload, headers)
     JSON.parse(res.body)
   end
 
