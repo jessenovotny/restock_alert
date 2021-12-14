84abef20662d1060e594e319922cac7dd15460b3
Jesse Novotny <jessenovotny@gmail.com>
2021-12-13 17:42:09 -0800

    re add scheduled task

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,9 +7,9 @@
 #
 # set :output, "/path/to/my/cron_log.log"
 #
-# every 1.minutes do
-#   rake "check_for_updates"
-# end
+every 1.minutes do
+  rake "check_for_updates"
+end
 #
 # every 4.days do
 #   runner "AnotherModel.prune_old_records"
